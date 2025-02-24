0d8fa64037c4ee1a63662c3287edd10cb505b700
Nick Michau <70205169+NOM1989@users.noreply.github.com>
2025-02-24 22:10:28 +0000

    add: animate between 2 hexes

--- src/hex.ts
+++ src/hex.ts
@@ -1,4 +1,25 @@
-const SIZE: number = 50
+
+const APP_WIDTH = 1000
+const APP_HEIGHT = 1000
+const HEX_SIZE = 50
+
+// Credit: redblobgames.com/grids/hexagons for Hex theory
+
+export class Point {
+  x: number
+  y: number
+
+  constructor(x: number, y: number) {
+    this.x = x
+    this.y = y
+  }
+
+  center() {
+    // Offset the point to align with Hex centers
+    this.x += HEX_SIZE
+    this.y += HEX_SIZE
+  }
+}
 
 export class Hex {
   q: number
@@ -13,21 +34,65 @@ export class Hex {
   }
 }
 
-export class Point {
-  x: number
-  y: number
-
-  constructor(x: number, y: number) {
-    this.x = x
-    this.y = y
-  }
+// Basic hex functions
+function hex_add(a: Hex, b: Hex) {
+  return new Hex(a.q + b.q, a.r + b.r);
 }
 
-export function pointy_hex_to_pixel(hex: Hex) {
-  const x = SIZE * (Math.sqrt(3) * hex.q  +  Math.sqrt(3)/2 * hex.r)
-  const y = SIZE * (3./2 * hex.r)
-  // return new Point(x, y)
+function hex_subtract(a: Hex, b: Hex) {
+    return new Hex(a.q - b.q, a.r - b.r);
+}
+
+function hex_multiply(a: Hex, k: number) {
+    return new Hex(a.q * k, a.r * k);
+}
+//
+function hex_length(hex: Hex) {
+    return Math.floor((Math.abs(hex.q) + Math.abs(hex.r) + Math.abs(hex.s)) / 2);
+}
+
+export function hex_distance(a: Hex, b: Hex) {
+    return hex_length(hex_subtract(a, b));
+}
+////
+
+
+export function pointy_hex_to_pixel(hex: Hex, hex_size: number, window_w: number, window_h: number) {
+  const x = hex_size * (Math.sqrt(3) * hex.q  +  Math.sqrt(3)/2 * hex.r) + ((window_w/2)-hex_size)
+  const y = hex_size * (3./2 * hex.r) + ((window_h/2)-hex_size)
   return {x, y}
 }
 
-export * from "./hex.ts"
+export function pointy_hex_to_pixel_POINT(hex: Hex) {
+  const x = HEX_SIZE * (Math.sqrt(3) * hex.q  +  Math.sqrt(3)/2 * hex.r) + ((APP_WIDTH/2)-HEX_SIZE)
+  const y = HEX_SIZE * (3./2 * hex.r) + ((APP_HEIGHT/2)-HEX_SIZE)
+  return new Point(x, y)
+}
+
+interface HashTable<T> {
+    [key: string]: T
+}
+
+export function hex_map_rect(left: number, right: number, top: number, bottom: number) {
+  let hexes: HashTable<Hex> = {}
+  for (let r = top; r <= bottom; r++) { // pointy top
+      const r_offset = Math.floor(r/2);
+      for (let q = left - r_offset; q <= right - r_offset; q++) {
+          hexes[`${q}${r}`] = new Hex(q, r)
+      }
+  }
+  return hexes
+}
+
+export function hex_map_hex(radius: number) {
+  let hexes: HashTable<Hex> = {}
+  for (let q = -radius; q <= radius; q++) {
+    const r1 = Math.max(-radius, -q - radius)
+    const r2 = Math.min(radius, -q + radius)
+    for (let r = r1; r <= r2; r++) {
+      hexes[`${q}${r}`] = new Hex(q, r)
+    }
+  }
+  return hexes
+}
+
--- src/main.ts
+++ src/main.ts
@@ -5,22 +5,126 @@ import * as HEX from './hex.ts'
 // Get the app div element
 const appDiv = document.querySelector<HTMLDivElement>('#app')!;
 
+const APP_WIDTH = 1000
+const APP_HEIGHT = 1000
+const HEX_SIZE = 50
+
 const app = new PIXI.Application()
-await app.init({ width: 1000, height: 1000 })
+await app.init({ width: APP_WIDTH, height: APP_HEIGHT })
 appDiv.appendChild(app.canvas)
 
 let hexContext = new PIXI.GraphicsContext()
-  .regularPoly(100, 100, 46, 6)
+  .regularPoly(HEX_SIZE, HEX_SIZE, HEX_SIZE-(Math.round(HEX_SIZE*0.08)), 6)
   // .circle(100, 100, 50)
   .fill('pink')
 
-for (let i = 0; i < 30; i++) {
-  let duplicate = new PIXI.Graphics(hexContext);
-  const index = Math.floor(i/5)
-  const hex = new HEX.Hex(Math.floor(i/10)-index+(i%5), index)
-  const { x, y } = HEX.pointy_hex_to_pixel(hex);
-  console.log("Drawing at: ", x, y)
-  duplicate.position.set(x, y)
-  app.stage.addChild(duplicate)
+// const hexes = HEX.hex_map_rect(-3, 3, -2, 2)
+const hexes = HEX.hex_map_hex(3)
+
+function draw_map(show_coords: boolean) {
+  Object.values(hexes).forEach(hex => {
+    let duplicate = new PIXI.Graphics(hexContext);
+    const { x, y } = HEX.pointy_hex_to_pixel(hex, HEX_SIZE, APP_WIDTH, APP_HEIGHT);
+    duplicate.position.set(x, y)
+    app.stage.addChild(duplicate)
+
+    // Optionally draw coordinates at the base of each hex
+    if ( show_coords ) {
+      const text = new PIXI.Text({
+        text: `${hex.q}.${hex.r}`,
+        style: {
+          fontFamily: 'Arial',
+          fontSize: 12,
+          fill: 0xff0000,
+          align: 'center',
+        }
+      })
+      text.position.set(
+        x + HEX_SIZE-Math.round(text.width/2),
+        y + Math.round(HEX_SIZE*1.5) -Math.round(text.height/2)
+      )
+      app.stage.addChild(text)
+    }
+  })
 }
 
+draw_map(true)
+
+let point = new PIXI.Graphics().circle(HEX_SIZE, HEX_SIZE, 10).fill('red')
+const { x, y } = HEX.pointy_hex_to_pixel(hexes["00"], HEX_SIZE, APP_WIDTH, APP_HEIGHT)
+point.position.set(x, y)
+app.stage.addChild(point)
+
+
+function animate_between_hexes(start_hex: HEX.Hex, end_hex: HEX.Hex) {
+  const elapsed_target = 50 // What duration to animate over
+
+  let start = HEX.pointy_hex_to_pixel_POINT(start_hex)
+  const end = HEX.pointy_hex_to_pixel_POINT(end_hex)
+
+  const x_diff = end.x - start.x
+  const y_diff = end.y - start.y
+
+  let elapsed = 0.0;
+
+  const animation_callback = (ticker: PIXI.Ticker) => {
+     elapsed += ticker.deltaTime;
+    point.x = x + (x_diff/elapsed_target)*Math.round(elapsed);
+    point.y = y + (y_diff/elapsed_target)*Math.round(elapsed);
+    if (elapsed >= elapsed_target) {
+      app.ticker.remove(animation_callback)
+    }
+  };
+
+  app.ticker.add(animation_callback)
+}
+
+animate_between_hexes(hexes["00"], hexes["1-1"])
+
+
+// function animate_between_hexes_v2(hex_path: HEX.Hex[]) {
+//   const total_elapsed_target = 100 // What duration to animate over
+//
+//   const steps = hex_path.length // How many sub-animations
+//
+//   let path_stages: PIXI.Point[] = []
+//
+//   const hex_coords = hex_path.map(hex => HEX.pointy_hex_to_pixel_POINT(hex))
+//
+//   for (let step = 0; step < steps-1; step++) {
+//     console.log(step)
+//     const start = hex_coords[step]
+//     const end = hex_coords[step+1]
+//
+//     const x_diff = end.x - start.x
+//     const y_diff = end.y - start.y
+//
+//     path_stages.push(new PIXI.Point(x_diff, y_diff))
+//   }
+//
+//   console.log(path_stages)
+//
+//   const elapsed_per_step_target = total_elapsed_target/steps
+//   
+//   let total_x = 0
+//   let total_y = 0
+//   let elapsed = 0.0;
+//   const animation_callback = (ticker: PIXI.Ticker) => {
+//     elapsed += ticker.deltaTime;
+//
+//     console.log(elapsed, elapsed_per_step_target, Math.floor(elapsed/elapsed_per_step_target))
+//
+//     total_x += path_stages[Math.floor(elapsed/elapsed_per_step_target)].x/elapsed_per_step_target
+//     total_y += path_stages[Math.floor(elapsed/elapsed_per_step_target)].y/elapsed_per_step_target
+//
+//     point.x = x + total_x
+//     point.y = y + total_y
+//     if (elapsed >= total_elapsed_target) {
+//       app.ticker.remove(animation_callback)
+//     }
+//   };
+//
+//   app.ticker.add(animation_callback)
+// }
+//
+// animate_between_hexes_v2([hexes["00"], hexes["1-1"], hexes["10"]])
